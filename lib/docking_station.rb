1db3158fe56bf9b60905ed1bb00ddc97493fe02b
Alex Lemons <alexlemons1@gmail.com>
2015-09-02 17:25:02 +0100

    dock method added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,10 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-    return Bike.new
+    #return Bike.new
+    fail "No bikes available"
+  end
+
+  def dock(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,24 @@
 require "docking_station"
 
 describe DockingStation do
-  it "responds to release_bike" do
-    expect(subject).to respond_to :release_bike
-  end
-  it "returns a working bike when asked to release_bike" do
-    expect(subject.release_bike).to be_a(Bike)
-    expect(subject.release_bike).to be_working
-  end
-end
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike) }
 
+    it "returns a working bike when asked to #release_bike" do
+      expect(subject.release_bike).to be_a(Bike)
+      expect(subject.release_bike).to be_working
+    end
+
+    it "raises an error when no bikes are available" do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
+
+
+  end
+
+
+  it "is expected to respond to the method dock" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+end

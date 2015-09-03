4551fd6a5d41704153854bcb7015ea4c35174b0e
Alex Lemons <alexlemons1@gmail.com>
2015-09-03 10:33:57 +0100

    Capacity added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,12 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-    #return Bike.new
     fail "No bikes available" unless @bike
     @bike
   end
 
   def dock(bike)
+    fail 'Docking Station FULL' if @bike
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,8 @@ describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
 
     it "returns a working bike when asked to #release_bike" do
-      subject.dock Bike.new
+      subject.dock(Bike.new)
+      expect{subject.dock Bike.new}.to raise_error "Docking Station FULL"
       expect(subject.release_bike).to be_a(Bike)
       expect(subject.release_bike).to be_working
     end
